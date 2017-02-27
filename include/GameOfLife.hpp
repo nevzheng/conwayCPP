013cf38e28407e7bb87e48a1688ea2a23b94f074
Nevin Zheng <nevzheng@gmail.com>
2017-02-27 01:55:09 -0500

    added include dir

---
+++ include/GameOfLife.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+#include <vector>
+
+typedef std::vector<std::vector<char>> conwayGrid;
+
+class GameOfLife {
+private:
+  int m_rows = 10;
+  int m_cols = 10;
+  conwayGrid m_grid;
+
+public:
+  // Constructors
+  GameOfLife();
+  GameOfLife(int m_rows, int m_cols);
+  GameOfLife(conwayGrid m_grid);
+
+  // destructor
+  ~GameOfLife() = default;
+
+  // getters
+  conwayGrid *get_conwayGrid();
+  std::pair<int, int> get_dimensions();
+
+  GameOfLife(const GameOfLife &other) = default;
+  // GameOfLife(GameOfLife &&other) = default;
+  // GameOfLife &operator=(const GameOfLife &other) = default;
+  // GameOfLife &operator=(GameOfLife &&other) = default;
+
+  // printing
+  friend std::ostream &operator<<(std::ostream &out, GameOfLife const &inst);
+
+  void randomInit();
+
+  void iterateGrid();
+
+  static void setCell(char &cell);
+  static void unsetCell(char &cell);
+  static char getState(char &cell);
+
+  static char getNeighborCount(char &cell);
+  static char countNeighbors(conwayGrid m_grid, int x, int y);
+  static void setNeighborCount(char &cell, char count);
+};
